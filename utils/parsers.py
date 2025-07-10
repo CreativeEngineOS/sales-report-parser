2365cd844bffa24420174a9e68032ff689df904c
Creative_Engine_OS <oogimages@gmail.com>
2025-07-10 11:17:36 -0400

    Update parsers.py

--- utils/parsers.py
+++ utils/parsers.py
@@ -1,12 +1,11 @@
 # utils/parsers.py
 
 import pandas as pd
-from nurphoto_parser import parse_nurphoto_pdf
+from utils.nurphoto_parser import parse_nurphoto_pdf  # ✅ Add "utils." prefix
 
 def parse_pdf(pdf_bytes, agency):
     if agency == "NurPhoto":
         df = parse_nurphoto_pdf(pdf_bytes)
         return df, "NurPhoto"
     
-    # Future support for other agencies like SIPA, Getty/iStock
     return None, agency
