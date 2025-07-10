0fdc0ab57f06c2bbb3e604fa47c0d6dea9465d4a
Creative_Engine_OS <oogimages@gmail.com>
2025-07-10 18:14:15 -0400

    Create __init__.py

---
+++ utils/__init__.py
@@ -0,0 +1,11 @@
+# This file marks the 'utils' directory as a Python package.
+# It can be left empty, or you can use it to expose key functions for convenience.
+
+# Example (optional): import common parsers for package-level access
+# from .getty_csv_parser import parse_getty_csv
+# from .getty_statement_parser import parse_getty_statement_csv
+# from .nurphoto_csv_parser import parse_nurphoto_csv
+# from .nurphoto_mhtml_parser import parse_nurphoto_mhtml
+# from .editorialfootage_csv_parser import parse_editorialfootage_csv
+# from .editorialfootage_mhtml_parser import parse_editorialfootage_mhtml
+# from .parsers import parse_pdf, detect_agency_from_text
