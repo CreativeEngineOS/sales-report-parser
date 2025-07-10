8fbfd10846b5d785a3655e0e6712835fed54f4c1
Creative_Engine_OS <oogimages@gmail.com>
2025-07-10 11:49:24 -0400

    Update parsers.py

--- utils/parsers.py
+++ utils/parsers.py
@@ -1,11 +1,15 @@
-# utils/parsers.py
-
 import pandas as pd
-from utils.nurphoto_parser import parse_nurphoto_pdf  # ✅ Add "utils." prefix
+from utils.nurphoto_parser import parse_nurphoto_pdf
+from utils.getty_parser import parse_getty_pdf  # this will be created
 
 def parse_pdf(pdf_bytes, agency):
     if agency == "NurPhoto":
         df = parse_nurphoto_pdf(pdf_bytes)
         return df, "NurPhoto"
-    
+
+    elif agency == "Getty/iStock":
+        df = parse_getty_pdf(pdf_bytes)
+        return df, "Getty/iStock"
+
+    # Placeholder for future agencies like SIPA
     return None, agency
